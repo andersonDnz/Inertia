a4c6de9621ca71619c0ee3bb9d8bd54bd5bc5660
anderson <andersondinizep@gmail.com>
2024-01-17 21:50:12 -0300

    Feat: Criando icone pointer

--- src/app/components/SlideComponent/index.tsx
+++ src/app/components/SlideComponent/index.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import React, { useState } from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled } from 'react-icons/rx';
 
 export const SlideComponent = () => {
   const slides = [
@@ -11,21 +14,48 @@ export const SlideComponent = () => {
     },
   ];
 
-  const backgroundImageStyle = {
-    backgroundImage: `url(${slides[0].url})`,
-    backgroundSize: 'cover',
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
   };
 
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex)
+  }
+
   return (
-    <div className='group  max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative'>
-      <div style={backgroundImageStyle} className='w-full h-full rounded-2xl bg-center duration-500'></div>
+    <div className='group max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative'>
+      <div style={{
+        backgroundImage: `url(${slides[currentIndex].url})`
+      }} className='w-full h-full rounded-2xl bg-center duration-500'></div>
 
       <div className='elemento hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2  bg-black/20 text-white cursor-pointer'>
-        <BsChevronCompactLeft size={30} />
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
       </div>
       <div className='elemento hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer '>
-        <BsChevronCompactRight size={30} />
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
       </div>
+
+      <div className='flex top-4 justify-center py-2'>
+        {slides.map((slides, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className='text-2xl cursor-pointer'>
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
+
     </div>
   );
-};
+}
