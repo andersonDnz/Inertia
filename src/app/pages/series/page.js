c37bf508c5519f4f925c223840427710e1b19209
anderson <andersondinizep@gmail.com>
2024-01-27 18:29:20 -0300

    Feat: Fazendo chamada api usando axios na pasta series

--- src/app/pages/series/page.js
+++ src/app/pages/series/page.js
@@ -1,35 +1,72 @@
-import React from 'react'
+'use client'
 
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-
+import axios from 'axios';
 
 const SeriesInfo = () => {
+  const [movieDetails, setMovieDetails] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      const options = {
+        method: 'GET',
+        url: 'https://movies-tv-shows-database.p.rapidapi.com/',
+        params: {
+          movieid: 'tt1375666'
+        },
+        headers: {
+          Type: 'get-movie-details',
+          'X-RapidAPI-Key': 'd64c24e974msh1b540dd739731cfp17cf45jsn5d6be5acb5e1',
+          'X-RapidAPI-Host': 'movies-tv-shows-database.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const response = await axios.request(options);
+        setMovieDetails(response.data);
+      } catch (error) {
+        console.error('Erro ao chamar a API:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchMovieDetails();
+  }, []);
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+
+
+
+
+
+
+
   return (
-    <div>
-      <div>
-        <h1>
-          Series
-        </h1>
-        <p>
-          Why do we use it?
-          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-        </p>
-        <p>
-          Why do we use it?
-          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-        </p>
-        <p>
-          Why do we use it?
-          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-        </p>
-      </div>
-      <div>
-        <Link href="/">
-          voltar
-        </Link>
+    <div className='min-h-screen flex items-center justify-center p-32 mb-6'>
+      <div className='max-w-5xl w-full mx-auto p-8 bg-white rounded-lg shadow-md text-center'>
+
+        {movieDetails && (
+          <div>
+            <h1 className='text-3xl font-bold mb-4'>{movieDetails.title}</h1>
+            <p className='text-gray-600 mb-4'>{movieDetails.description}</p>
+
+          </div>
+        )}
+
+        <div className='mr-[100%] p-2 px-2 border w-20 border-blue-600 bg-blue-400 rounded-full text-lg'>
+          <Link href="/">
+            Voltar
+          </Link>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default SeriesInfo;
