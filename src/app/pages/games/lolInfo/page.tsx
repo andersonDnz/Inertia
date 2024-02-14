bd07f01589dbdf8c877bad34c1a8fe92406c2ad9
anderson <andersondinizep@gmail.com>
2024-02-13 21:30:54 -0300

    Feat: Criando componente gotCharacters

--- src/app/pages/games/lolInfo/page.tsx
+++ src/app/pages/games/lolInfo/page.tsx
@@ -1,38 +1,69 @@
+'use client'
 
+import React, { useState, useEffect } from 'react';
 
-import React, { useEffect, useState } from 'react'
+interface Character {
+  id: number;
+  firstName: string;
 
-const LolRancking = () => {
-
-  {/*const [loading, setLoading] = useState(false);
-const [data, setData] = useState(null)
-
-  useEffect(() => {
-    const fetchLol = async () => {
-      const url = 'https://league-of-legends-galore.p.rapidapi.com/api/getLoLRanks';
-      const options = {
-        method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': 'd64c24e974msh1b540dd739731cfp17cf45jsn5d6be5acb5e1',
-          'X-RapidAPI-Host': ' bearer league-of-legends-galore.p.rapidapi.com'
-        }
-      };
-
-      try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchLol();
-  }, []);*/}
-
-
-  return (
-    <div>LolRancking</div>
-  )
 }
 
-export default LolRancking;
+const GotCharacters: React.FC = () => {
+  const [data, setData] = useState<Character[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchData = async () => {
+    const url = 'https://game-of-thrones1.p.rapidapi.com/Characters';
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'd64c24e974msh1b540dd739731cfp17cf45jsn5d6be5acb5e1',
+        'X-RapidAPI-Host': 'game-of-thrones1.p.rapidapi.com',
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+
+      if (response.ok) {
+        const result: Character[] = await response.json();
+
+        setData(result);
+      } else {
+        console.error('Erro na solicitação:', response.status, response.statusText);
+        setError(`Erro na solicitação: ${response.status} ${response.statusText}`);
+      }
+    } catch (error) {
+      console.error('Erro na solicitação:', error);
+
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className='min-h-screen flex items-center justify-center p-32'>
+      {data ? (
+
+        <ul>
+          {data.map((character) => (
+            <li key={character.id}>{character.firstName}</li>
+          ))}
+        </ul>
+      ) : (
+
+        error ? (
+
+          <p>{error}</p>
+        ) : (
+
+          <p>Carregando...</p>
+        )
+      )}
+    </div>
+  );
+};
+
+export default GotCharacters;
